37298989d2480fec351fb5c47744520c17aae146
moayadnajd <moayad.najd@gmail.com>
2019-09-04 19:07:22 +0300

    update

--- RES/src/configs/dashboard.ts
+++ RES/src/configs/dashboard.ts
@@ -98,46 +98,6 @@ export const dashboardConfig: GeneralConfigs[] = [
   },
   {
     show: true,
-    class: 'col-md-9 mt-3 no-side-padding',
-    component: 'MapComponent',
-    componentConfigs: {
-      id: 'map',
-      source: 'country',
-      title: 'Info Products Overview',
-      description: `
-          Geographic tags for all the information products found are represented here and disaggregated by
-          country. The darker the color the higher the number of information products tagged to that specific
-          country. Overall, the graphic shows the world areas targeted by research activities that produced
-          information products. Click on ICONS:view_headline
-          to export this graphic, click on ICONS:expand_less
-          to collapse it.
-      `
-    } as ComponentDashboardConfigs,
-    scroll: {
-      icon: 'map'
-    },
-    tour: true
-  },
-  {
-    class: 'col-md-3 mt-3 no-side-padding shorter',
-    show: true,
-    component: 'ListComponent',
-    componentConfigs: {
-      id: 'mapTop',
-      title: 'Top Countries',
-      source: 'country',
-      description: `
-          The top Countries by number of information products.
-          Click on ICONS:expand_less to collapse the list.
-      `
-    } as ComponentDashboardConfigs,
-    scroll: {
-      linkedWith: 'map'
-    },
-    tour: true
-  },
-  {
-    show: true,
     class: 'col-md-12 mt-3 no-side-padding',
     component: 'BarComponent',
     componentConfigs: {
@@ -181,44 +141,6 @@ export const dashboardConfig: GeneralConfigs[] = [
     show: true,
     component: 'ListComponent',
     componentConfigs: {
-      id: 'topAffiliations',
-      title: 'Top Affiliations',
-      source: 'affiliation',
-      description: `
-          Top twenty affiliations by number of information products.
-          Click on ICONS:expand_less to collapse the list.
-      `
-    } as ComponentDashboardConfigs,
-    scroll: {
-      linkedWith: 'topLists'
-    },
-    tour: true
-  },
-  {
-    class: 'col-md-6 mt-3 no-side-padding',
-    show: true,
-    component: 'ListComponent',
-    componentConfigs: {
-      id: 'CRP',
-      title: 'CRPs and Platforms',
-      source: 'crp',
-      description: `
-          All CRPs and platforms tagged across all information products
-          are represented here and ordered by quantity of tags for
-          each CRP or platform. Scroll down to see more results.
-          Click on ICONS:expand_less to collapse the list.
-      `
-    } as ComponentDashboardConfigs,
-    scroll: {
-      icon: 'star_rate'
-    },
-    tour: true
-  },
-  {
-    class: 'col-md-6 mt-3 no-side-padding',
-    show: true,
-    component: 'ListComponent',
-    componentConfigs: {
       id: 'funders',
       title: 'Funders',
       source: 'sponsorship',
@@ -229,7 +151,7 @@ export const dashboardConfig: GeneralConfigs[] = [
       `
     } as ComponentDashboardConfigs,
     scroll: {
-      linkedWith: 'CRP'
+      linkedWith: 'topLists'
     },
     tour: true
   },
@@ -250,16 +172,13 @@ export const dashboardConfig: GeneralConfigs[] = [
         icon: 'repo',
         identifierUri: 'uri',
         altmetric: true,
-        description: 'citation',
+        description: 'abstract',
         tags: {
           Publisher: 'publisher',
           Subject: 'subject',
           Type: 'type',
-          Status: 'status',
           'Date issued': 'date',
-          'Reporting CRP(s) and Platform(s)': 'crp',
           'Attention Score': 'altmetric',
-          numbers: 'numbers'
         },
         filterOptions: [
           { display: 'Date', value: 'date', sort: 'desc' },
@@ -272,11 +191,6 @@ export const dashboardConfig: GeneralConfigs[] = [
           {
             display: 'Altmetric: Attention Score',
             value: 'altmetric.score',
-            sort: 'desc'
-          },
-          {
-            display: 'Views & Downloads',
-            value: 'numbers.score',
             sort: 'desc'
           }
         ]
