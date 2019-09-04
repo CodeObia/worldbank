34dba1ebb6893be9c7f634b1fb206c449cb45d61
moayadnajd <moayad.najd@gmail.com>
2019-09-04 14:48:40 +0300

    first commit

---
+++ RES/src/app/dashboard/components/chart/chart.component.ts
@@ -0,0 +1,65 @@
+import {
+  Component,
+  Input,
+  ViewChild,
+  ElementRef,
+  HostListener,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { Observable } from 'rxjs';
+import * as Highcharts from 'highcharts';
+import wordCloudModule from 'highcharts/modules/wordcloud';
+import ExportingModule from 'highcharts/modules/exporting';
+import BoostModule from 'highcharts/modules/boost';
+import MapModule from 'highcharts/modules/map';
+wordCloudModule(Highcharts);
+ExportingModule(Highcharts);
+MapModule(Highcharts);
+BoostModule(Highcharts);
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ChartComponent {
+  @Input() isMap = false;
+  @Input() title: string;
+  @Input() description: string;
+  @Input() expandedStatus: boolean;
+  @Input() userSeesMe: boolean;
+  @Input() loading: boolean;
+  @Input() loadingHits$: Observable<boolean>;
+  @Input() chartOptions: Highcharts.Options;
+  @Output() expanded: EventEmitter<boolean>;
+  @Output() chartInstance: EventEmitter<Highcharts.Chart>;
+  @ViewChild('clickToEnable') clickToEnable: ElementRef;
+  Highcharts = Highcharts;
+  constructor() {
+    this.expanded = new EventEmitter<boolean>();
+    this.chartInstance = new EventEmitter<Highcharts.Chart>();
+  }
+
+  notifyExpanded(b: boolean): void {
+    this.expanded.emit(b);
+  }
+
+  hideClickToEnable(): void {
+    this.clickToEnable.nativeElement.hidden = true;
+  }
+
+  @HostListener('mouseleave', ['$event'])
+  onMouseMove(e: MouseEvent): void {
+    if (this.clickToEnable) {
+      // pie and worldcould do not have this
+      this.clickToEnable.nativeElement.hidden = false;
+    }
+  }
+
+  logChartInstance(e: Highcharts.Chart): void {
+    this.chartInstance.emit(e);
+  }
+}
